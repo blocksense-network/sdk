c4807838be83a56b003f60683f3641c97a5734cf
Yordan Madzhunkov <y.madzhunkov@gmail.com>
2024-10-11 11:19:58 +0300

    Update SKD with changes from 10 Oct 2024

--- src/oracle.rs
+++ src/oracle.rs
@@ -1,24 +1,44 @@
+use serde::{Deserialize, Serialize};
+
 #[derive(Clone, Debug)]
 pub struct DataFeedSetting {
     pub id: String,
     pub data: String,
 }
 
+#[derive(Clone, Debug)]
+pub struct Capability {
+    pub id: String,
+    pub data: String,
+}
+
 pub struct Settings {
     pub data_feeds: Vec<DataFeedSetting>,
+    pub capabilities: Vec<Capability>,
 }
 
 impl Settings {
-    pub fn new(data_feeds: Vec<DataFeedSetting>) -> Self {
-        Self { data_feeds }
+    pub fn new(data_feeds: Vec<DataFeedSetting>, capabilities: Vec<Capability>) -> Self {
+        Self {
+            data_feeds,
+            capabilities,
+        }
     }
+}
+
+#[derive(Deserialize, Serialize, Clone, Debug)]
+pub enum DataFeedResultValue {
+    None,
+    Numerical(f64),
+    Text(String),
+    Error(String),
 }
 
 //TODO(adikov): Start using FeedType from feed_registry
 #[derive(Debug)]
 pub struct DataFeedResult {
     pub id: String,
-    pub value: f64,
+    pub value: DataFeedResultValue,
 }
 
 #[derive(Debug, Default)]
